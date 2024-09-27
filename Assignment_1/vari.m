3f5e52f6bed4574bac2e11d4ce1e8721c05c0b6a
Kyuubi5054 <geckotamer@icloud.com>
2024-09-27 14:23:04 +0000

    Completed assignment

---
+++ Assignment_1/vari.m
@@ -0,0 +1,21 @@
+% Author: Sam Falk
+% Email: falksa78@rowan.edu
+% Created On: 09/25/24
+% Updated On: 09/25/24
+% All Rights Reserved
+
+radius = 5; % This variable stores the constant for the predetermined radius
+
+area = 3.14 * (radius^2); % This equation calculates the area of the circle
+
+circumference = 2 * 3.14 * radius; % This variable calculates the circumference of the circle
+
+
+fprintf ('The area of the circle is: %g \n', area); % Displays the solution to the area equation
+
+fprintf ('The circumference of the circle is: %g \n', circumference); % Displays the solution to the circumference equation
+
+% execute in terminal
+% git add Assignment_1/vari.m
+% git commit -m 'finished vari'
+% git push origin main
